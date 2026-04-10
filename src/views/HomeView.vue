682028cf6f6fced63b4b1fde2e0a9a8d4fe26621
ocean <372092104@qq.com>
2026-04-10 14:30:04 +0800

    feat: initialize Vue 3 project with Vite, Pinia, and TypeScript configuration

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import Custompage from '../components/Custompage.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <Custompage />
+  </main>
+</template>
