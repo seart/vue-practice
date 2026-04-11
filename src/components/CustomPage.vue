ca675fcba8edebb27bb4110b435c44b0e85bcff0
ocean <372092104@qq.com>
2026-04-11 12:33:38 +0800

    if 条件 for 列表循环

--- src/components/CustomPage.vue
+++ src/components/CustomPage.vue
@@ -1,101 +1,24 @@
 <script setup lang="ts">
-import { ref, reactive, onMounted, onUnmounted } from 'vue'
 
-const count = ref(1)
+import { reactive, ref } from 'vue'
 
-let timere: ReturnType<typeof setInterval> | null = null
+const list = ref(['hell', 'world', 'one', 'two', 'three'])
 
-const stopTimer = () => {
-  if (timere) {
-    clearInterval(timere)
-  }
-}
-
-const startTimer = () => {
-  stopTimer()
-  timere = setInterval(() => {
-    count.value += 1
-  }, 1000)
-}
-
-onMounted(() => {
-  startTimer()
+const object = reactive({
+  name: 'vue',
+  age: 12,
+  sex: 'W',
 })
-
-onUnmounted(() => {
-  stopTimer()
-})
-
-const pageTitle = ref('hello')
-// 反转字符串
-const inverseTitle = () => {
-  pageTitle.value = pageTitle.value.split('').reverse().join('')
-}
-
-const show = ref(true)
-// 隐藏/显示
-const hideShow = () => {
-  show.value = !show.value
-}
-
-// --------------------------------------------------
-
-const listItem = ref(['apple', 'paer', 'orange'])
-const inputText = ref('')
-const add = () => {
-  if (inputText.value == '') {
-    return
-  }
-  listItem.value.push(inputText.value)
-  inputText.value = ''
-}
-
-const remove = () => {
-  listItem.value.pop()
-}
 </script>
 
 <template>
   <div class="custom">
-    {{ count }}
+    <template v-for="(item, index) in list" :key="index">
+      <div v-if="item !== 'one'">{{ item }} -- {{ index }}</div>
+    </template>
 
-    <span v-if="show">{{ pageTitle }}</span>
-    <button v-on:click="inverseTitle">inverse</button>
-    <button v-on:click="hideShow">hide/show</button>
-    <div></div>
-
-    <ul>
-      <li v-for="(item, index) in listItem" :key="index">{{ index + 1 }}.{{ item }}</li>
-    </ul>
-    <div>
-      <input type="text" v-model="inputText" />
-      <button v-on:click="add">add</button>
-      <button v-on:click="remove">remove</button>
+    <div v-for="(value, key, index) in object" :key="index">
+      <div v-if="key !== 'age'">{{ key }}:{{ value }}--{{ index }}</div>
     </div>
   </div>
 </template>
-
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
