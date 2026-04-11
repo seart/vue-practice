4480f579c2a7858a5e7c22f338b53995b65ed012
ocean <372092104@qq.com>
2026-04-11 16:14:53 +0800

    修饰符，表单双向绑定的指令

--- src/components/CustomPage.vue
+++ src/components/CustomPage.vue
@@ -1,32 +1,46 @@
 <script setup lang="ts">
 import { reactive, ref } from 'vue'
 
-const list = ref(['hell', 'world', 'one', 'two', 'three'])
-const object = reactive({
-  name: 'vue',
-  age: 12,
-  sex: 'W',
-})
+const checkbox =ref(true);
 
-const count = ref(0)
-const handleBtnClick = (event) => {
-  console.log('event:', event)
-  count.value += 1
-}
+const inputText = ref('haha');
+
+
+const message = ref('');
+
+
+const inputTextModifiers = ref('');
 </script>
 
 <template>
   <div class="custom">
-    <template v-for="(item, index) in list" :key="index">
-      <div v-if="item !== 'one'">{{ item }} -- {{ index }}</div>
-    </template>
 
-    <div v-for="(value, key, index) in object" :key="index">
-      <div v-if="key !== 'age'">{{ key }}:{{ value }}--{{ index }}</div>
+    <div>
+      {{checkbox}}
+      <input type="checkbox" v-model="checkbox" />
     </div>
 
-    <div>{{ count }}</div>
-    <button @click="handleBtnClick">add number</button>
+    <div>
+      {{inputText}}
+      <input v-model="inputText" />
+    </div>
+
+  <div>
+    {{ message }}
+    <select v-model="message">
+      <option disabled value="">请选择</option>
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>
+  </div>
+
+  <div>
+    {{typeof inputTextModifiers}} --
+  {{ inputTextModifiers }}
+    <input v-model.trim.number.lazy="inputTextModifiers" />
+  </div>
+
 
   </div>
 </template>
