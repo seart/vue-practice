29d55ab3d6a9570604537d1a0d17fed52a452fad
ocean <372092104@qq.com>
2026-04-11 14:20:42 +0800

    事件绑定跳过就不做了，有需要查看官方文档

--- src/components/CustomPage.vue
+++ src/components/CustomPage.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
-
 import { reactive, ref } from 'vue'
 
 const list = ref(['hell', 'world', 'one', 'two', 'three'])
-
 const object = reactive({
   name: 'vue',
   age: 12,
   sex: 'W',
 })
+
+const count = ref(0)
+const handleBtnClick = (event) => {
+  console.log('event:', event)
+  count.value += 1
+}
 </script>
 
 <template>
@@ -20,5 +24,9 @@ const object = reactive({
     <div v-for="(value, key, index) in object" :key="index">
       <div v-if="key !== 'age'">{{ key }}:{{ value }}--{{ index }}</div>
     </div>
+
+    <div>{{ count }}</div>
+    <button @click="handleBtnClick">add number</button>
+
   </div>
 </template>
